2f8b72dcb3625c060ae6f67141d33689c74c34b4
joyaAmora <49413284+joyaAmora@users.noreply.github.com>
2019-11-21 20:44:14 -0500

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -31,4 +31,78 @@ WHERE c.NCLI = cm.NCLI AND cm.codeTx = t.codeTx
 ORDER BY c.NCLI, cm.NCOM;
 
 -- 3c
-select QCOM 
+select NPRO, QSTOCK
+FROM PRODUIT
+WHERE QSTOCK < qMin;
+
+-- 3d
+
+-- 3e
+
+-- 3f 
+select c.NCLI, nom, NCOM, (totalSansTx+(totalSansTx*(TPS+TVQ))) as total
+FROM CLIENT c LEFT JOIN COMMANDE cm on c.NCLI = cm.NCLI, TAXE t;
+
+
+-- 5
+    --a) La commande 99999 n'existe pas erreur de clé étrangère, insérer dans une commande existante ex: 30178
+    --b) Duplicata de la clé primaire, changer la clé primaire F011 pour une non existante ex: G011
+    --c) Ne peut pas supprimer le parent avant l'enfant (DETAIL a encore des data), faire un déclencheur afin d'assurer la propagation de la suppression de COMMANDE à DETAIL
+
+-- 6
+
+    -- Sur delete d'une commande non payée
+    DELIMITER |
+    CREATE TRIGGER suppCommande BEFORE DELETE
+    ON COMMANDE FOR EACH ROW
+        BEGIN
+            IF(old.payee = 0)
+                UPDATE CLIENT
+                    SET COMPTE -= (old.totalSansTx + old.totalSansTx*(TPS+TVQ))
+                    FROM TAXE t
+                    WHERE old.codeTx = t.codeTx
+                    AND old.NCLI = CLIENT.NCLI;
+            END IF;
+        END;
+    END |
+    DELIMITER ;
+
+    -- Sur UPDATE totalSansTx
+    DELIMITER |
+    CREATE TRIGGER updateTotal AFTER UPDATE
+    ON COMMANDE FOR EACH ROW
+        BEGIN
+            IF(old.payee = 0)
+                UPDATE CLIENT
+                    SET COMPTE -= (old.totalSansTx + old.totalSansTx*(TPS+TVQ))
+                    FROM TAXE t
+                    WHERE old.codeTx = t.codeTx
+                    AND old.NCLI = CLIENT.NCLI;
+            END IF;
+            IF (new.payee = 0)
+                UPDATE CLIENT
+                        SET COMPTE += (new.totalSansTx + new.totalSansTx*(TPS+TVQ))
+                        FROM TAXE t
+                        WHERE new.codeTx = t.codeTx
+                        AND new.NCLI = CLIENT.NCLI;
+            END IF;
+        END;
+    END |
+    DELIMITER ;
+
+
+    -- Sur UPDATE boolean payee inutile car gérée dans le trigger du dessus
+   /* DELIMITER |
+    CREATE TRIGGER updatePayee AFTER UPDATE
+    ON COMMANDE FOR EACH ROW
+        BEGIN
+            IF(old.payee = 0 AND new.payee = 1) -- pour s'assurer qu'une commande passe de non payée a payée
+                UPDATE CLIENT
+                    SET COMPTE = COMPTE - old.totalSansTx + old.totalSansTx*(TPS+TVQ)
+                    FROM TAXE t
+                    WHERE old.codeTx = t.codeTx
+                    AND old.NCLI = CLIENT.NCLI;
+            END IF;
+        END;
+    END |
+    DELIMITER ; */
